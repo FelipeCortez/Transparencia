35f90510bca31a4930b05986f477fba890eec0c1
rubensaraujo <rubensaviana@hotmail.com>
2014-06-08 20:22:08 -0300

    Visualização de processos por parte do usuário e index temporario

--- transparencia/app/controllers/Application.java
+++ transparencia/app/controllers/Application.java
@@ -7,14 +7,23 @@ import views.html.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok("Your new application is ready. Sr. - ");
+	public static Result index() {
+        return ok(views.html.app_index.render());
     }
     
     public static Result verParlamentar(Long id){
         return controllers.Parlamentar.appVerParlamentar(id);
     }
+    
     public static Result sessoes() {
     	return controllers.Sessao.appPesqSessoes();
+    }
+
+    public static Result listarParlamentar(){
+        return controllers.Parlamentar.appListarParlamentar();
+    }
+    
+    public static Result visualizarProcessos(Long id){
+        return controllers.Processo.appVisualizarProcesso(models.Parlamentar.find.byId(id));
     }
 }
